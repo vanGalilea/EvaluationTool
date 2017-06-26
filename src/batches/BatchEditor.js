16e598f65751831696841f15ff8c575d98772761
vanGalilea <z.galili@icloud.com>
2017-06-26 19:30:56 +0200

    batch editor comp. with validation func

--- src/batches/BatchEditor.js
+++ src/batches/BatchEditor.js
@@ -16,6 +16,7 @@ class BatchEditor extends PureComponent {
       startDate,
       endDate,
       students,
+      errors: {},
     }
   }
 
@@ -37,6 +38,24 @@ class BatchEditor extends PureComponent {
     })
   }
 
+  validate(batch) {
+    const { number, startDate, endDate } = batch
+
+    let errors = {}
+
+    if (!number) errors.number = 'Make sure you give a number'
+    if (number < 0) errors.number = 'Please try to cooperate and fill a positive number'
+    if (!startDate || startDate === '') errors.startDate = "Don't forget the start date"
+    if (!endDate || endDate === '') errors.endDate = "Don't forget the end date"
+    if (endDate < startDate) errors.dates = "The periode of dates doesn't make sense: make sure that the start date is before the end date"
+
+    this.setState({
+      errors,
+    })
+
+    return Object.keys(errors).length === 0
+  }
+
   saveBatch() {
     const {
       number,
@@ -52,11 +71,14 @@ class BatchEditor extends PureComponent {
       students,
     }
 
-    this.props.createBatch(batch)
-    console.log(batch)
+    if (this.validate(batch)) {
+      this.props.createBatch(batch)
+    }
   }
 
   render() {
+    const { errors } = this.state
+
     return (
       <div className="editor">
         <input
@@ -65,7 +87,10 @@ class BatchEditor extends PureComponent {
           className="number"
           placeholder="Batch No."
           defaultValue={this.state.number}
-          onChange={this.updateNumber.bind(this)} />
+          onChange={this.updateNumber.bind(this)}
+          onKeyDown={this.updateNumber.bind(this)} />
+
+        { errors.number && <p className="error">{ errors.number }</p> }
 
         <input
           type="date"
@@ -73,7 +98,10 @@ class BatchEditor extends PureComponent {
           className="startDate"
           placeholder="Start Date"
           defaultValue={this.state.startDate}
-          onChange={this.updateStartDate.bind(this)} />
+          onChange={this.updateStartDate.bind(this)}
+          onKeyDown={this.updateStartDate.bind(this)} />
+
+        { errors.startDate && <p className="error">{ errors.startDate }</p> }
 
         <input
           type="date"
@@ -81,7 +109,11 @@ class BatchEditor extends PureComponent {
           className="endDate"
           placeholder="End Date"
           defaultValue={this.state.endDate}
-          onChange={this.updateEndDate.bind(this)} />
+          onChange={this.updateEndDate.bind(this)}
+          updateStartDate={this.updateEndDate.bind(this)} />
+
+        { errors.endDate && <p className="error">{ errors.endDate }</p> }
+        { errors.dates && <p className="error">{ errors.dates }</p> }
 
         <div className="actions">
           <button className="primary" onClick={this.saveBatch.bind(this)}>Save</button>
